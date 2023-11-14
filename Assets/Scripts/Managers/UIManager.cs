9ad1037a8e3ac222f81fc69de30dd8177c9ed632
springik <springik.games@email.cz>
2023-11-14 17:55:18 +0100

    feat:56/added basic UIManager logic
    -added UIManager
    -added UIController

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour, IManager
 {
+    static UIManager _instance;
+    public static UIManager Instance => _instance;
+
+    List<UIController> controllers = new List<UIController>();
     private void Awake()
     {
-        GameManager.Instance.AddManager(this);
+        if(_instance == null)
+        {
+            _instance = this;
+            GameManager.Instance.AddManager(Instance);
+        }
     }
     public void Stop()
     {
+        //TODO: Implement
         throw new System.NotImplementedException();
     }
     void IManager.Start()
     {
+        //TODO: Implement
         throw new System.NotImplementedException();
+    }
+    public void AddController(UIController controller)
+    {
+        controllers.Add(controller);
+    }
+    void HideUI()
+    {
+        for (int i = 0; i < controllers.Count; i++)
+        {
+            controllers[i].HideUI();
+        }
     }
 }
---
+++ Assets/Scripts/UIScripts/UIController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public void HideUI()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
