0678cd3dc6ab1846c073e7404e4767e9cce5374b
springik <springik.games@email.cz>
2023-11-14 20:00:27 +0100

    feat:56/added a way to add UIControllers to Manager
    -I fucked up and there was no way to actually add the controllers to the manager oops

--- Assets/Scripts/UIScripts/BaseUIController.cs
+++ Assets/Scripts/UIScripts/BaseUIController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public abstract class BaseUIController
 {
     [SerializeField] protected Canvas _uiRoot;
+    protected virtual void AddToManager()
+    {
+        UIManager.Instance.AddController(this);
+    }
     public abstract void HideUI();
     public abstract void ShowUI();
     public abstract void UpdateUI();
