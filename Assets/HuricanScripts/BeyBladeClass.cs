a281311235f677c70186007ea2fd80ea09d2a2de
HuricanSpeed <mcg03241@gmail.com>
2023-12-12 13:33:36 +0100

    feat: 102/beyblade selection

---
+++ Assets/HuricanScripts/BeyBladeClass.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Beyblade
+{
+    public string name;
+    public Sprite image; // Pøedpokládáme, že používáte 2D obrázky
+}
