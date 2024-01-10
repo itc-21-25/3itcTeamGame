559a9ec3ceedaf0b218cb2629caeb2eebdb8ca0e
Adam Doležal <dolezal.adam@merkatos.cz>
2024-01-10 09:51:45 +0100

    feat: gitignore and PAM

--- Assets/Scripts/Player/PlayerAudioManager.cs
+++ Assets/Scripts/Player/PlayerAudioManager.cs
@@ -4,27 +4,82 @@ using UnityEngine;
 
 public class PlayerAudioManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource _AudioSource = null;
+
+    // Music
+    [SerializeField] private AudioSource _MusicAudioSource = null;
+
+    [SerializeField] private List<AudioClip> _BackgroundMusic = new List<AudioClip>();
+    [SerializeField] private List<AudioClip> _FightMusic = new List<AudioClip>();
+
+    // SFX
+    [SerializeField] private AudioSource _SfxAudioSource = null;
+
+    [SerializeField] private List<AudioClip> _FightCollisionSFX = new List<AudioClip>();
+
+    // Death audio
+    [SerializeField] private AudioSource _DeathAudioSource = null;
+
+
     [SerializeField] private List<AudioClip> _DeadAudioClips = new List<AudioClip>();
+
+
+
+
 
     public void Init()
     {
 
     }
 
-    public void PlayDeadAudio()
+    //Music
+
+    public void PlayBackgroundMusic()
     {
-        if (!_AudioSource.isPlaying)
+        if (!_MusicAudioSource.isPlaying)
         {
-            _AudioSource.clip = _DeadAudioClips[Random.Range(0, _DeadAudioClips.Count)];
-            _AudioSource.Play();
+            _MusicAudioSource.clip = _BackgroundMusic[Random.Range(0, _BackgroundMusic.Count)];
+            _MusicAudioSource.Play();
+        }
+    }
+
+    public void PlayFightMusic()
+    {
+        if (!_MusicAudioSource.isPlaying)
+        {
+            _MusicAudioSource.clip = _FightMusic[Random.Range(0, _FightMusic.Count)];
+            _MusicAudioSource.Play();
         }
     }
 
 
+    // DEATH AUDIO
+
+    public void PlayDeadAudio()
+    {
+        if (!_DeathAudioSource.isPlaying)
+        {
+            _DeathAudioSource.clip = _DeadAudioClips[Random.Range(0, _DeadAudioClips.Count)];
+            _DeathAudioSource.Play();
+        }
+    }
+
+
+    // SFX
+
+    public void PlayFightCollisionSFX()
+    {
+        if (!_SfxAudioSource.isPlaying)
+        {
+            _SfxAudioSource.clip = _FightCollisionSFX[Random.Range(0, _FightCollisionSFX.Count)];
+            _SfxAudioSource.Play();
+        }
+    }
+
+ 
+
     public void OnCanvasGroupChanged()
     {
-        if(_AudioSource != null )
+        if(_MusicAudioSource != null & _SfxAudioSource != null & _DeathAudioSource != null )
         {
 
         }
