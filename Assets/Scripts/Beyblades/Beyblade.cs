c449d53f6e11eaeb0cf09d34bca78e72e45b9605
Párek8 <polakpetr8878@gmail.com>
2024-01-10 09:53:34 +0100

    Feat: beyblade component scriptableobject

---
+++ Assets/Scripts/Beyblades/Beyblade.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "/BeybladeComponents/Component")]
+public class Beyblade : ScriptableObject
+{
+    [field: SerializeField] int Damage;
+    [field: SerializeField] int Stamina;
+    [field: SerializeField] int Speed;
+    [field: SerializeField] int Weight;
+    [field: SerializeField] GameObject Prefab;
+
+
+}
